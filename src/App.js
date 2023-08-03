35ca9f5854327c26e984826669935b31c8d85b89
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-08-02 23:49:28 -0400

    add gtags

--- src/App.js
+++ src/App.js
@@ -8,82 +8,98 @@ import InfoCheck from "./components/InfoCheck";
 import AppointmentList from "./routes/AppointmentList";
 
 export default function App() {
-  const [token, setStateToken] = useState("");
-  const [name, setName] = useState("");
-  const [dependents, setDependents] = useState(null);
-  const [locations, setLocations] = useState(null);
+	const [token, setStateToken] = useState("");
+	const [name, setName] = useState("");
+	const [dependents, setDependents] = useState(null);
+	const [locations, setLocations] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getLocations();
-      setLocations(data.locations);
-    }
-    fetchData();
-  }, []);
+	useEffect(() => {
+		async function fetchData() {
+			const data = await getLocations();
+			setLocations(data.locations);
+		}
+		fetchData();
+	}, []);
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const queryToken = urlParams.get("token");
-    if (queryToken != null) {
-      localStorage.setItem("token", queryToken);
-    }
-    const token = localStorage.getItem("token");
-    setStateToken(token);
-  }, []);
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const queryToken = urlParams.get("token");
+		const utm_source = urlParams.get("utm_source");
+		const utm_medium = urlParams.get("utm_medium");
+		const utm_campaign = urlParams.get("utm_campaign");
+		const gclid = urlParams.get("gclid");
+		const fbclid = urlParams.get("fbclid");
+		const queryGoogleTag = {
+			utm_source,
+			utm_medium,
+			utm_campaign,
+			gclid,
+			fbclid,
+		};
+		if (queryToken != null) {
+			localStorage.setItem("token", queryToken);
+		}
+		console.log(queryGoogleTag);
+		if (!Object.values(queryGoogleTag).every((x) => x == null)) {
+			localStorage.setItem("queryGoogleTag", JSON.stringify(queryGoogleTag));
+		}
+		const token = localStorage.getItem("token");
+		setStateToken(token);
+	}, []);
 
-  function setToken(token) {
-    localStorage.setItem("token", token);
-    setStateToken(token);
-  }
+	function setToken(token) {
+		localStorage.setItem("token", token);
+		setStateToken(token);
+	}
 
-  return (
-    <main className={`${styles.main} card`}>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<SelectAppointment locations={locations} token={token} />}
-          />
-          <Route
-            path="/schedule"
-            element={
-              <InfoCheck
-                name={name}
-                setName={setName}
-                setDependents={setDependents}
-                dependents={dependents}
-                token={token}
-                setToken={setToken}
-              >
-                <ScheduleAppointment
-                  name={name}
-                  dependents={dependents}
-                  setDependents={setDependents}
-                  locations={locations}
-                />
-              </InfoCheck>
-            }
-          />
-          <Route
-            path="/appointment-list"
-            element={
-              <InfoCheck
-                name={name}
-                setName={setName}
-                setDependents={setDependents}
-                dependents={dependents}
-                token={token}
-                setToken={setToken}
-              >
-                <AppointmentList
-                  dependents={dependents}
-                  locations={locations}
-                />
-              </InfoCheck>
-            }
-          />
-        </Routes>
-      </Router>
-    </main>
-  );
+	return (
+		<main className={`${styles.main} card`}>
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						element={<SelectAppointment locations={locations} token={token} />}
+					/>
+					<Route
+						path="/schedule"
+						element={
+							<InfoCheck
+								name={name}
+								setName={setName}
+								setDependents={setDependents}
+								dependents={dependents}
+								token={token}
+								setToken={setToken}
+							>
+								<ScheduleAppointment
+									name={name}
+									dependents={dependents}
+									setDependents={setDependents}
+									locations={locations}
+								/>
+							</InfoCheck>
+						}
+					/>
+					<Route
+						path="/appointment-list"
+						element={
+							<InfoCheck
+								name={name}
+								setName={setName}
+								setDependents={setDependents}
+								dependents={dependents}
+								token={token}
+								setToken={setToken}
+							>
+								<AppointmentList
+									dependents={dependents}
+									locations={locations}
+								/>
+							</InfoCheck>
+						}
+					/>
+				</Routes>
+			</Router>
+		</main>
+	);
 }
--- src/api/appointments.js
+++ src/api/appointments.js
@@ -3,21 +3,24 @@ import api from "./index";
 const BASE_URL = "/appointments";
 
 export function getAppointments() {
-  return api.get(`${BASE_URL}/recent`);
+	return api.get(`${BASE_URL}/recent`);
 }
 
 export function postCancelAppointment(appointmentId) {
-  return api.post(`${BASE_URL}/cancel`, { appointmentId });
+	return api.post(`${BASE_URL}/cancel`, { appointmentId });
 }
 
 export function postCreateAppointment({ location, time, dependent = null }) {
-  return api.post(`${BASE_URL}/schedule`, {
-    location,
-    time,
-    ...(dependent != null && { dependent }),
-  });
+	const googleTag = JSON.parse(localStorage.getItem("queryGoogleTag"));
+	localStorage.removeItem("queryGoogleTag");
+	return api.post(`${BASE_URL}/schedule`, {
+		location,
+		time,
+		...(dependent != null && { dependent }),
+		...(googleTag != null && { googleTag }),
+	});
 }
 
 export function postRescheduleAppointment({ appointmentId, location, time }) {
-  return api.post(`${BASE_URL}/reschedule`, { appointmentId, location, time });
+	return api.post(`${BASE_URL}/reschedule`, { appointmentId, location, time });
 }
